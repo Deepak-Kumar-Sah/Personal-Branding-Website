3a7d8b82fa495a926a2f69bb3b814d9a0d91be11
Simrangupta27 <sg.simrangupta91@gmail.com>
2020-07-29 16:48:01 +0530

    Changed

---
+++ src/Component/Skills/skills.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import demo from './Assets/images/demo1.jpg';
+import './skills.css';
+
+export default class skill extends React.Component{
+    render(){
+        return(
+            
+        )
+    }
+}
