7ec5a4b7324d115b08fd875f46539117037a2ce5
riyasinghal20 <garimasinghal076@gmail.com>
2020-08-02 18:16:35 +0530

    riya about

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Homepage from "./Component/FirstSection/Homepage";
-import SimpleSlider from "./Component/BackgroundImage/BackgroundImage"
-import * as serviceWorker from './serviceWorker';
+import About from "./Component/about/About";
+// import SimpleSlider from "./Component/BackgroundImage/BackgroundImage"
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Homepage />
+    <About/>
     {/* <SimpleSlider/> */}
   </React.StrictMode>,
   document.getElementById('root')
@@ -16,4 +16,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
