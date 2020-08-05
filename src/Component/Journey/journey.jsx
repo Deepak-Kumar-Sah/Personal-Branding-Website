cb7cffda435aef010913a6a96c7e7b564d4713f6
Deepak-Kumar-Sah <deepaksah9998@gmail>
2020-08-05 11:26:22 +0530

    journey

--- src/Component/Journey/journey.jsx
+++ src/Component/Journey/journey.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './journey.css';
 import hello from "./journey.jpg";
+import {
+    Link
+  } from 'react-router-dom';
 export default class Journey  extends React.Component{
     render(){
         return(
             <div className ="hello" >
             <img src={hello}/>
             <div className="centered">BE A PART OF MY JOURNEY </div> 
-            <button class="buton">Read More... </button>
+            <button class="buton"><Link to="/journey" className="journey-anchor">Read More...</Link></button>
          </div>
          );
         }
